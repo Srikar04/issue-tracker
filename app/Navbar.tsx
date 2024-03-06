269d922635e18afce146ed44ec709ea840682128
srikar <msrikar0403@gmail.com>
2024-03-06 12:44:29 +0530

    style active bar and fixed multiple rendering of navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Link from 'next/link';
-import React from 'react'
+import React,{useEffect} from 'react'
 import { IoBugSharp } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 
 const Navbar = () => {
@@ -8,12 +12,34 @@ const Navbar = () => {
     {href:"/",label:"DashBoard"},
     {href:"/issues",label:"Issues"}
   ]
+  const [isClientRendered, setIsClientRendered] = React.useState(false);
+
+  let pathname: String = usePathname();
+
+  useEffect(() => {
+    setIsClientRendered(true);
+  }, []);
+
+  useEffect(() => {
+    if (isClientRendered) {
+      console.log(pathname);
+    }
+  }, [pathname, isClientRendered]);
+  
   return (
     <nav className="flex space-x-6 px-5 h-14 mb-5 items-center border-b-4">
         <Link href="/"><IoBugSharp /></Link>
         <ul className="flex space-x-6">
             {arr.map((obj)=> 
-                <li className="text-zinc-500 hover:text-zinc-900" key={obj.label}>
+                <li 
+                className={classNames({
+                            'text-zinc-900': pathname === obj.href,
+                            'text-zinc-500': pathname !== obj.href,
+                            'hover:text-zinc-900 transition-colors': true
+                        }
+                    )
+                } 
+                key={obj.label}>
                     <Link href={obj.href}>{obj.label}</Link>
                 </li>
             )}
