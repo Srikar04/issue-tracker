cf49432c9fca36f8d8d6e055de2aff6cd2cd2b42
srikar <msrikar0403@gmail.com>
2024-03-11 23:16:36 +0530

    update Location of CustomLink

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,7 @@
 import prisma from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
 import IssueStatusBadge from '../components/IssueStatusBadge';
-import CustomLink from './CustomLink';
+import CustomLink from '../components/CustomLink';
 import IssueActions from './IssueActions';
 import delay from 'delay';
 
