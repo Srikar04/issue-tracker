9d748a85bd49d5432ab9fcd66c7b7de840da49c8
srikar <msrikar0403@gmail.com>
2024-03-11 12:52:57 +0530

    added loading skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import {Button,Table} from '@radix-ui/themes';
-import Link from 'next/link';
-import { PrismaClient } from '@prisma/client'
-import IssueStatusBadge from '../components/IssueStatusBadge';
-import delay from 'delay';
-import IssueActions from './IssueActions';
+import { Table } from '@radix-ui/themes'
+import React from 'react'
+import IssueActions from './IssueActions'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 
-const Issues = async () => {
-  const prisma = new PrismaClient();
-  const allIssues = await prisma.issue.findMany();
-  
-  await delay(2000);
-
+const loading = () => {
+  const allIssues = [1,2,3,4,5]
   return (
-    <div>
+      <div>
        <IssueActions />
        <Table.Root variant='surface'>
           <Table.Header>
@@ -26,17 +20,17 @@ const Issues = async () => {
           </Table.Header>
           <Table.Body>
             {allIssues.map((issue) => (
-              <Table.Row key={issue.id}>
+              <Table.Row key={issue}>
                 <Table.Cell>
-                  {issue.title}
+                  <Skeleton />
                   <div className="block md:hidden">
-                    <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                   </div>
                 </Table.Cell>
                 <Table.Cell className="hidden md:table-cell">
-                  <IssueStatusBadge status={issue.status} />
+                    <Skeleton />
                 </Table.Cell>
-                <Table.Cell className="hidden md:table-cell">{issue.createdAt.toDateString()}</Table.Cell>
+                <Table.Cell className="hidden md:table-cell"><Skeleton /></Table.Cell>
               </Table.Row>
             ))}
           </Table.Body>
@@ -45,4 +39,4 @@ const Issues = async () => {
   )
 }
 
-export default Issues
+export default loading
