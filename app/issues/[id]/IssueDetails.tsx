7f8930753745d2f1cc5a60f65b9500146c9d3fc6
srikar <msrikar0403@gmail.com>
2024-03-13 12:05:12 +0530

    Implemented SRP (Single Responsibility Principle)

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,22 @@
+import { IssueStatusBadge } from '@/app/components'
+import { Heading, Flex, Card, Text } from '@radix-ui/themes'
+import React from 'react'
+import Markdown from 'react-markdown'
+import {Issue}  from '@prisma/client'
+
+const IssueDetails = ({issue} : {issue : Issue}) => {
+  return (
+    <div>
+          <Heading>{issue.title}</Heading>
+          <Flex gap="3" my="3">
+              <IssueStatusBadge status={issue.status} />
+              <Text>{issue.createdAt.toDateString()}</Text>
+          </Flex>
+          <Card className="prose" my="5">
+              <Markdown>{issue.description}</Markdown>
+          </Card>
+    </div>
+  )
+}
+
+export default IssueDetails
