c955424df0d42c459ee0f82bd24b29bb9422625e
srikar <msrikar0403@gmail.com>
2024-03-14 18:27:13 +0530

    Delete Issue button working

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,22 @@
 'use client';
 
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
+
+    // Note: We have to import the useRouter hook from next/navigation
+    // not from next/router because we are using the new app structure
+    const router = useRouter();
+
+    const deleteIssue = async () => {
+        await axios.delete(`/api/issues/${issueId}`);
+        router.push('/issues');
+        router.refresh();
+    }
+
     return (
         <AlertDialog.Root>
             <AlertDialog.Trigger>
@@ -21,7 +34,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
                         </Button>
                     </AlertDialog.Cancel>
                     <AlertDialog.Action>
-                        <Button variant="solid" color="red">
+                        <Button variant="solid" color="red" onClick={deleteIssue}>
                             Delete Issue
                         </Button>
                     </AlertDialog.Action>
