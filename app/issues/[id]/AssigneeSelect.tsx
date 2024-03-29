c8cf693d1716db9d7b85054c64617ebcccd94e54
srikar <msrikar0403@gmail.com>
2024-03-29 20:48:42 +0530

    Created api for users (get) and react useState and useEffect to show that

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,15 +2,32 @@
 
 import React from 'react'
 import { Select } from "@radix-ui/themes";
+import { User } from '@prisma/client';
+import axios from 'axios';
 
 const AssigneeSelect = () => {
+    const [users, setUsers] = React.useState<User[]>([]);
+
+    React.useEffect(() => {
+        const fetchUsers = async () => {
+            const { data } = await axios.get<User[]>("/api/users");
+            setUsers(data);
+        }
+        fetchUsers();
+    }, []);
+
+
     return (
         <Select.Root>
             <Select.Trigger placeholder="Assign to User" />
             <Select.Content>
                 <Select.Group>
                     <Select.Label>Suggestions</Select.Label>
-                    <Select.Item value="srikar">Srikar</Select.Item>
+                    {
+                        users.map((user) => {
+                            return <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>
+                        })
+                    }
                 </Select.Group>
             </Select.Content>
         </Select.Root>
