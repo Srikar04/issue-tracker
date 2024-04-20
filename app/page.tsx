b73af6f5c0f8d18f20759150147df04d11a41ee6
srikar <msrikar0403@gmail.com>
2024-04-20 18:41:42 +0530

    Redirecting users to /issues/list

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
-import Issues from './issues/list/page';
+'use client';
+
+import { useRouter } from "next/navigation";
+import {useEffect} from "react";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/issues/list');
+  },[router])
   return (
     <main>
-      <Issues />
+       Coming soon..
     </main>
   );
 }
